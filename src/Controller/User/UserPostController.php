04bbc0b5340c5158de9be40b2b83601d87b0b8a5
Gonzalez <gonzalezlouis1981@gmail.com>
2024-04-10 10:52:11 +0200

    FIX BUGS regression

--- src/Controller/User/UserPostController.php
+++ src/Controller/User/UserPostController.php
@@ -4,9 +4,12 @@ namespace App\Controller\User;
 
 use App\Entity\Post;
 use App\Form\PostType;
+use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Entity\UploadFile;
 use App\Service\FileUploader;
 use App\Repository\PostRepository;
+use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -36,29 +39,20 @@ class UserPostController extends AbstractController
             'posts' => $post,
         ]);
     }
-
-
     #[Route('/new', name: 'app_user_post_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager, FileUploader $fileUploader): Response
     {
         $post = new Post();
         $post->setAuthor($this->getUser());
-        
-
         $form = $this->createForm(PostType::class, $post);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $imgFile = $form->get('img')->getData();
-            
             if ($imgFile) {
-                $imgFileName = $fileUploader->upload($imgFile, "img_directory", false);
+                $fileUpload = $fileUploader->upload($imgFile, "img_directory", false);
                 // updates the 'imgFilename' property to store the PDF file name
                 // instead of its contents
-                $fileUpload = new UploadFile();
-                $fileUpload->setImg($imgFileName);
-                $fileUpload->setCreatedAt(new \DateTimeImmutable());
-                $fileUpload->setModifiedAt(new \DateTimeImmutable());
             }
             $entityManager->persist($fileUpload);
             $post->setImg($fileUpload);
@@ -73,28 +67,54 @@ class UserPostController extends AbstractController
             'form' => $form,
         ]);
     }
-
     /*
     * SLUGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
     */
     #[Route('/{slug}', name: 'app_user_post_by_slug', methods: ['GET'])]
-    public function showBySlug(PostRepository $postRepository, string $slug): Response
+    public function showBySlug(Post $post, PostRepository $postRepository, Request $request, EntityManagerInterface $entityManager, CommentRepository $commentRepository, string $slug): Response
     {
         // return $this->render('post/show.html.twig', [
         //     'post' => $postRepository->findOneBy(['slug' => $slug]),
         // ]);
+        $comment = new Comment();
+        $comment->setUser($this->getUser());
+        $comment->setPost($post);
 
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_post_by_slug', ['slug' =>$slug]);
+        }
         $post = $postRepository->findBySlug($slug);
+        $comments = $commentRepository->findBy(['post' => $post]);
         return $this->render('post/show.html.twig', [
             'post' => $post,
+            'comments' => $comments,
+            'form' => $form
         ]);
     }
 
     #[Route('/show/{id}', name: 'app_user_post_show', methods: ['GET'])]
-    public function show(Post $post): Response
+    public function show(Post $post, CommentRepository $commentRepository, Request $request, EntityManagerInterface $entityManager): Response
     {
+        $comment = new Comment();
+        $comment->setUser($this->getUser());
+        $comment->setPost($post);
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_post_by_slug', ['slug' =>$slug]);
+        }
+        $comments = $commentRepository->findBy(['post' => $post]);
         return $this->render('post/show.html.twig', [
             'post' => $post,
+            'comments' => $comments,
+            'form' => $form
         ]);
     }
 
@@ -110,7 +130,6 @@ class UserPostController extends AbstractController
             
             if ($imgFile) {
                 $imgFileName = $fileUploader->upload($imgFile, "img_directory");
-
                 // updates the 'imgFilename' property to store the PDF file name
                 // instead of its contents
                 $fileUpload = new UploadFile();
