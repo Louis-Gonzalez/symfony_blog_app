14fcab884b89c7b3a0dbfdd1bfa51a15e82e6315
louis Gonzalez <gonzalezlouis1981@gmail.com>
2024-07-30 09:28:16 +0200

    UDT Form Comment

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -9,14 +9,16 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('description')
-        ;
+            ->add('description', TextareaType::class, [
+                'attr' => ['rows' => 5],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
