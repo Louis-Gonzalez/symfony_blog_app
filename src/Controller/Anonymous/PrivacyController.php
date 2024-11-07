2bcba617e6fbc171249fe262cd04326126ef9ac3
louis Gonzalez <gonzalezlouis1981@gmail.com>
2024-11-07 12:35:24 +0100

    FIX policy and ADD CGU

--- src/Controller/Anonymous/PrivacyController.php
+++ src/Controller/Anonymous/PrivacyController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller\Anonymous;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use APY\BreadcrumbTrailBundle\Annotation\Breadcrumb;
 use APY\BreadcrumbTrailBundle\BreadcrumbTrail\Trail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PrivacyController extends AbstractController
 {
-    #[Route('/privacy', name: 'app_anonymous_privacy')]
+    #[Route('privacy', name: 'app_anonymous_privacy')]
     #[Breadcrumb(title:'Home', routeName: 'app_home')]
-    public function index(Trail $trail): Response
+    public function showPolicy(Request $request,Trail $trail): Response
     {
         $trail->add('Privacy', 'app_anonymous_privacy');
-        return $this->render('anonymous/privacy/index.html.twig');
+        $locale = $request->getLocale();
+        return $this->render('anonymous/privacy/policy.html.twig', [
+            'locale' => $locale,
+        ]);
+    }
+
+    #[Route('privacy/cgu', name: 'app_anonymous_privacy_cgu')]
+    #[Breadcrumb(title:'Home', routeName: 'app_home')]
+    public function showCgu(Request $request, Trail $trail): Response
+    {
+        $trail->add('CGU', 'app_anonymous_privacy_cgu');
+        $locale = $request->getLocale();
+        return $this->render('anonymous/privacy/cgu.html.twig', [
+            'locale' => $locale,
+        ]);
     }
 }
